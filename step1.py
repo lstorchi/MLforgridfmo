f2b9c2c73d36d2d4c0ba3f07ee3c02c8d7f058ea
Loriano Storchi <loriano@storchi.org>
2019-12-20 12:38:37 +0100

    add also correlation and more

--- step1.py
+++ step1.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import numpy as nm
+
+import scipy.stats
+import os.path
 import sys
+
 
 #######################################################################
 
@@ -14,34 +19,110 @@ def difference(lst1, lst2):
 
 #######################################################################
 
-filename = ""
+def readfile(filename, labelname):
 
-if len(sys.argv) != 2:
-    print("usage: " , sys.argv[0] , " file.xslx")
-    exit(1)
-else:
-    filename = sys.argv[1]
+    xls = pd.ExcelFile(filename)
 
-xls = pd.ExcelFile(filename)
+    names = []
+    features = {}
+    labels = []
 
-names = []
-features = []
+    thefirst = True
+    for name in xls.sheet_names:
+        print ("Parsing data from ", name)
+        df = xls.parse(name)
+        ft = list(df.columns.values)
+        print("  reading names from ", ft[0])
 
-thefirst = True
-for name in xls.sheet_names:
-    print ("Parsing data from ", name)
-    df = xls.parse(name)
-    ft = list(df.columns.values)
-    print("  reading names from ", ft[0])
-    if thefirst:
-        names = list(df[ft[0]].values)
-        thefirst = False
-    else:
-        localnames = list(df[ft[0]].values)
-        if len(names) != len(localnames):
-            print("  set of names is different ")
-            print("  ", len(names) , " vs ", len(localnames))
-            print("  ", difference(localnames, names))
-            print("  ", difference(names, localnames))
+        morethen = []
+        if thefirst:
+            names = list(df[ft[0]].values)
+            thefirst = False
+        else:
+            localnames = list(df[ft[0]].values)
+            if len(names) != len(localnames):
+
+                morethen = difference(localnames, names)
+                lessthen = difference(names, localnames)
+
+                if len(morethen):
+                    print("    WARNING has more entries ", morethen, " I will drop it")
+                elif len(lessthen):
+                    print("    ERROR has less entries ", lessthen)
+                    exit(1)
+
+        dfrn = df.set_index(ft[0], drop = True)
+
+        if len(morethen) > 0:
+            dfrn = dfrn.drop(morethen)
+
+        for vn in dfrn.columns.values:
+            if vn != labelname:
+                features[vn] = dfrn.loc[:,vn].values
+            else:
+                labels =  dfrn.loc[:,vn].values
+
+
+    # check values 
+    dim = len(labels)
+
+    if (dim != len(names)):
+        print ("ERROR in dimensions")
+        exit(1)
+
+    for k in features:
+        if dim != features[k].shape[0]:
+            print ("ERROR in dimensions of ", k, " ", dim , " vs ", features[k].shape[0])
             exit(1)
 
+    return names, features, labels
+
+#######################################################################
+
+if __name__== "__main__":
+    
+    filename = ""
+
+    if len(sys.argv) != 2:
+        print("usage: " , sys.argv[0] , " file.xslx")
+        exit(1)
+    else:
+        filename = sys.argv[1]
+
+    labelname = "logKa"
+
+    names = []
+    features = {}
+    labels = []
+
+    if os.path.exists(filename):
+        names, features, labels = readfile(filename, labelname)
+    else:
+        print("file ", filename , " does not exist ")
+
+    for k in features:
+
+        m = nm.mean(features[k])
+        d = nm.std(features[k])
+
+        #print(k, " has mean: ", m, " and STD: ", d)
+
+        features[k] = features[k] - m
+        features[k] = features[k] / d
+
+    for k1 in features:
+        for k2 in features:
+            if k1 != k2:
+                P = scipy.stats.pearsonr(features[k1], features[k2])[0]
+                S = scipy.stats.spearmanr(features[k1], features[k2])[0]
+                #K = scipy.stats.kendalltau(features[k1], features[k2])[0]
+                
+                if abs(P) > 0.5 or abs(S) > 0.5:
+                    print ("\"",k1, "\" vs \"", k2, "\" corr P,S: ", P, S)
+
+    for k in features:
+        P = scipy.stats.pearsonr(features[k], labels)[0]
+        S = scipy.stats.spearmanr(features[k], labels)[0]
+
+        if abs(P) > 0.5 or abs(S) > 0.5:
+            print ("\"",k, "\" vs ", labelname, " corr P,S: ", P, S)
