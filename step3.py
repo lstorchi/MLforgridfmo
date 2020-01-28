e1d9735a116486cdb36b429455876d7f93128cdb
Loriano Storchi <loriano@storchi.org>
2020-01-28 13:46:31 +0100

    add also Pearson R

--- step3.py
+++ step3.py
@@ -1,6 +1,10 @@
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
 from sklearn.tree import DecisionTreeRegressor  
+from sklearn.metrics import mean_squared_error
 from sklearn.cluster import KMeans
+
+from scipy.stats import pearsonr
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -10,7 +14,37 @@ import scipy.stats
 import os.path
 import sys
 
+import math
+
 import util
+
+#####################################################################
+
+def rf_split_build_and_test (features_array, labels):
+
+     X_train, X_test, y_train, y_test = train_test_split( \
+             features_array, labels, test_size=0.10)
+     
+     regressor = DecisionTreeRegressor(random_state = 0)  
+       
+     # fit the regressor with X and Y data 
+     regressor.fit(X_train, y_train) 
+     
+     y_pred = regressor.predict(X_test) 
+     
+     #for i in range(y_pred.shape[0]):
+     #    print(y_pred[i], " ", y_test[i])
+     
+     rms = math.sqrt(mean_squared_error(y_test, y_pred))
+     mae = mean_absolute_error(y_test, y_pred)
+     absdiff = [abs(x - y) for x, y in zip(y_test, y_pred)]
+     maxae = np.amax(absdiff)
+
+     rp = pearsonr(y_test, y_pred)[0]
+
+     return rms, mae, maxae, rp, y_test, y_pred
+
+#####################################################################
 
 if __name__== "__main__":
     
@@ -71,16 +105,28 @@ if __name__== "__main__":
 
         #split tran and test set
 
-        X_train, X_test, y_train, y_test = train_test_split( \
-                features_array, labels, test_size=0.10)
+        mean_rms = 0.0
+        mean_mae = 0.0
+        mean_maxae = 0.0
+        mean_rp = 0.0
+        num_of_run = 200
 
-        regressor = DecisionTreeRegressor(random_state = 0)  
-          
-        # fit the regressor with X and Y data 
-        regressor.fit(X_train, y_train) 
+        for i in range(num_of_run):
 
-        y_pred = regressor.predict(X_test) 
+           rms, mae, maxae, rp, y_pred, y_test = \
+                   rf_split_build_and_test (features_array, labels)
+           
+           #print (" RMSE: ", rms)
+           #print ("  MAE: ", mae)
+           #print ("MaxAE: ", maxae)
 
-        for i in range(y_pred.shape[0]):
-            print(y_pred[i], " ", y_test[i])
+           mean_rms += rms
+           mean_mae += mae
+           mean_maxae += maxae
+           mean_rp += rp
+
+        print ("Mean  RMSE: ", mean_rms/float(num_of_run))
+        print ("Mean   MAE: ", mean_mae/float(num_of_run))
+        print ("Mean MaxAE: ", mean_maxae/float(num_of_run))
+        print ("Mean    rP: ", mean_rp/float(num_of_run))
 
